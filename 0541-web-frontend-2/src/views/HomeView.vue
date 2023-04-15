8805dcd8806ba0588006635a99e0eee0f44a53a2
Alfonsdev <106644852+AlfonsInfo@users.noreply.github.com>
2023-04-15 14:36:35 +0700

    homepage

--- 0541-web-frontend-2/src/views/HomeView.vue
+++ 0541-web-frontend-2/src/views/HomeView.vue
@@ -3,7 +3,20 @@
 </script>
 
 <template>
-  <main>
-    <h1>Selamat Datang diaplikasi Gofit</h1>
-  </main>
+  <div class="welcome-message">
+    <h1>Selamat Datang Di Aplikasi Gofit</h1>
+  </div>
+  <div class="login-form">
+      
+  </div>
 </template>
+
+<style scoped>
+  .welcome-message{
+    padding: 10px;
+    border-radius: 5px;
+    background-color: rgba(0,0,0,0.8);
+    color: white;
+  }
+
+</style>
