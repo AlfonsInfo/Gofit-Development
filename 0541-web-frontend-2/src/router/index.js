9d793f18eb6fe45a060e9f2a3fcc2d2334cb5252
Alfonsdev <106644852+AlfonsInfo@users.noreply.github.com>
2023-04-15 23:31:32 +0700

    p 3 el

--- 0541-web-frontend-2/src/router/index.js
+++ 0541-web-frontend-2/src/router/index.js
@@ -13,12 +13,37 @@ const router = createRouter({
     {
       path: '/Home',
       name: 'Home',
+      meta : {
+        requiresAuth : true
+      },
+      beforeEnter: (to, from, next) => {
+        if (isLoggedIn()) { // fungsi isLoggedIn() akan mengembalikan true jika pengguna sudah terautentikasi
+          next();
+        } else {
+          next('/'); // redirect ke halaman login jika pengguna belum terautentikasi
+        }
+      },
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
+    },
+      // route fallback
+    {
+      path: '/:pathMatch(.*)*',
+      redirect: '/'
     }
   ]
 })
+
+//Munkin nanti dipindahkan ke helper function
+function isLoggedIn()
+{
+    let token  =localStorage.getItem('access_token')
+    console.log(token)
+    if(token != null)
+      return true;
+    return false;
+}
 
 export default router
