9c95793804b24726923e6a0088693a49fde71a4e
Alfonsdev <106644852+AlfonsInfo@users.noreply.github.com>
2023-04-15 14:06:31 +0700

    bersihkan code default vue

--- 0541-web-frontend-2/src/views/HomeView.vue
+++ 0541-web-frontend-2/src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h1>Selamat Datang diaplikasi Gofit</h1>
   </main>
 </template>
