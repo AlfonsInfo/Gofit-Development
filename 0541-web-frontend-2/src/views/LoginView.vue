f772a9d26ed3636a4f65d31d88788271d5b422c1
Alfonsdev <106644852+AlfonsInfo@users.noreply.github.com>
2023-04-17 15:29:55 +0700

    model lain

--- 0541-web-frontend-2/src/views/LoginView.vue
+++ 0541-web-frontend-2/src/views/LoginView.vue
@@ -1,71 +1,75 @@
 <script>
-import axios from 'axios' ;
+import axios from 'axios';
 import { defineComponent, reactive } from 'vue';
-import { useRouter} from 'vue-router';
+import { useRouter } from 'vue-router';
+
+const togglePassword = (showPassword) => {
+  return showPassword ? 'text' : 'password';
+}
+
+const toggleIconPassword = (showPassword) => {
+  return showPassword ? 'mdi mdi-eye-off' : '';
+}
+
+const useLogin = () => {
+  const router = useRouter();
+  const inputLogin = reactive({
+    username: '',
+    password: '',
+  });
+  const showPassword = reactive(false);
+
+  const login = async () => {
+    const request = {
+      username: inputLogin.username,
+      password: inputLogin.password,
+    }
+    const postUrl = 'http://127.0.0.1:8000/api/login';
+
+    try {
+      const response = await axios.post(postUrl, request);
+
+      // Destructuring Login Response
+      const { user, access_token, pegawai } = response.data;
+
+      // Set Local Storage
+      localStorage.setItem('token', access_token);
+      localStorage.setItem('userData', JSON.stringify(user));
+      localStorage.setItem('pegawaiData', JSON.stringify(pegawai));
+
+      //Route ke Home
+      router.push({ name: 'Home' });
+    } catch (e) {
+      alert('Gagal Login');
+    }
+  }
+
+  return {
+      login,
+    inputLogin,
+    showPassword,
+    togglePassword,
+    toggleIconPassword,
+  }
+}
 
 export default defineComponent({
-  mounted(){
+  mounted() {
     document.body.classList.add('body-center');
   },
-  setup(){
-    const inputLogin = reactive({
-      username : "",
-      password : "",
-    });
-    
-    const router = useRouter('router'); //tidak boleh dalam fungsi login karena fungsi login await(event callback)
-    let showPassword = false;
+  setup() {
+    const { login, inputLogin, showPassword,togglePassword,toggleIconPassword } = useLogin();
 
-    const Login = async () => {
-      console.log('Fungsi Login');
-      let username = inputLogin.username;
-      let password = inputLogin.password;
-
-      const request = {
-        username : username,
-        password : password
-      }
-      const postUrl ='http://127.0.0.1:8000/api/login' 
-
-      try{
-
-        //* Login Request
-        const Login = await axios.post(postUrl,request);
-
-        // Destructuring Login Response
-        let {user, access_token,pegawai} = Login.data;
-        
-        // Set Local Storage
-        localStorage.setItem('token', access_token);
-        localStorage.setItem('userData', JSON.stringify(user));
-        localStorage.setItem('pegawaiData', JSON.stringify(pegawai))
-        
-        //Route ke Home
-        router.push({name: "Home"});
-      }catch(e)
-      {
-        alert('Gagal Login')
-        // console.log(e)
-      }
-    
-    }
-
-    const togglePassword = () => {
-      return showPassword ? 'text' : 'password'
-    }
-
-    const toggleIconPassword = () => {
-      return showPassword ? 'mdi mdi-eye-off' : ''
-    }
-    return{
-      Login,
+    return {
+      login,
       inputLogin,
-      togglePassword,
-      toggleIconPassword,
       showPassword,
+      togglePassword,
+      toggleIconPassword
     }
   }
 })
+
 </script>
 
 <template style>
@@ -86,8 +90,8 @@ export default defineComponent({
       <div class="mb-3">
         <label for="password" class="form-label">Password</label>
         <div class="password-wrapper">
-          <input :type="togglePassword()" class="form-control" id="password"  v-model="inputLogin.password" required>
-          <i span toggle="#password" class="toggle-password icon-blue"  :class ="toggleIconPassword()"></i>
+          <input :type="togglePassword" class="form-control" id="password"  v-model="inputLogin.password" required>
+          <i span toggle="#password" class="toggle-password icon-blue"  :class ="toggleIconPassword"></i>
         </div>
       </div>
       <button type="submit" class="btn btn-primary">Submit</button>
