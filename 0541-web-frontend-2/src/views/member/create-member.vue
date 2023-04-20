b1e9820d0ba99f5d003a6a5182d4c9f1535d8913
Alfonsdev <106644852+AlfonsInfo@users.noreply.github.com>
2023-04-20 22:12:22 +0700

    custom date picker

--- 0541-web-frontend-2/src/views/member/create-member.vue
+++ 0541-web-frontend-2/src/views/member/create-member.vue
@@ -43,8 +43,11 @@
             <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
             <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
           </div>
-
-          <div>Tanggal Lahir Member</div>
+          <div class="mb-3">
+            <label class="form-label" for="birthday">Tanggal Lahir Member  </label>
+            <input class="form-text" type="date" id="birthday" name="birthday">
+          </div>
+          <div></div>
           <div class="mb-3">
             <label for="exampleInputPassword1" class="form-label">No Telp Member</label>
             <input type="password" class="form-control" id="exampleInputPassword1">
@@ -59,7 +62,46 @@
 
 <style scoped>
 
+/* set width to match other form inputs */
+input[type="date"] {
+  width: 100%;
+  padding: 0.375rem 0.75rem;
+  font-size: 1rem;
+  font-weight: 400;
+  line-height: 1.5;
+  color: #495057;
+  background-color: #fff;
+  background-clip: padding-box;
+  border: 1px solid #ced4da;
+  border-radius: 0.25rem;
+  transition: border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
+}
 
+/* hide default arrow */
+input[type="date"]::-webkit-calendar-picker-indicator {
+  display: none;
+}
+
+/* use Bootstrap icon for date picker */
+input[type="date"]::before {
+  content: "\f0ed";
+  font-family: "Material Design Icons";
+  font-weight: 400;
+  font-size: 1.25rem;
+  line-height: 1.5;
+  color: #6c757d;
+  position: absolute;
+  right: 0.75rem;
+  top: 50%;
+  transform: translateY(-50%);
+  pointer-events: none;
+}
+
+/* change border color on focus */
+input[type="date"]:focus {
+  border-color: #80bdff;
+  box-shadow: 0 0 0 0.2rem rgba(0, 123, 255, 0.25);
+}
 
 
 .content{
